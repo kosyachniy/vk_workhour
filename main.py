e4883e045fd8daf4df76cc17534bff2bef076866
Alex Poloz <alexypoloz@gmail.com>
2023-01-07 23:14:33 +0700

    Messages & Work time & Holidays & User name

--- main.py
+++ main.py
@@ -3,13 +3,22 @@ import datetime
 
 from libdev.cfg import cfg
 
-from lib.vk import read, send
+from lib.vk import read, send, info
 
 
 tzinfo=datetime.timezone(datetime.timedelta(hours=cfg('tz')))
 
 while True:
     timestamp = time.time()
+    current_time = datetime.datetime.fromtimestamp(timestamp, tz=tzinfo)
+
+    if current_time.weekday() < 5 and (
+        9 < current_time.hour < 18
+        or (current_time.hour == 9 and current_time.minute > 40)
+        or (current_time.hour == 18 and current_time.minute < 30)
+    ):
+        time.sleep(300)
+        continue
 
     try:
         new_message = read()
@@ -24,14 +33,12 @@ while True:
             continue
 
         message_time = datetime.datetime.fromtimestamp(i[3], tz=tzinfo)
+        name = info(i[0]).get('name', 'Привет')
 
-        if message_time.weekday() < 5 and (
-            9 < message_time.hour < 18
-            or (message_time.hour == 9 and message_time.minute > 30)
-            or (message_time.hour == 18 and message_time.minute < 30)
-        ):
+        if current_time.weekday() >= 5:
+            send(i[0], cfg('message2').format(name))
             continue
 
-        send(i[0], 'чё надо')
+        send(i[0], cfg('message1').format(name))
 
     time.sleep(120)
